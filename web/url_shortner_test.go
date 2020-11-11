d26b7db44af671258d3d28f99a4201136d9b6d8a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-11 11:32:42 +0530

    examples

--- web/url_shortner_test.go
+++ web/url_shortner_test.go
@@ -6,6 +6,14 @@ import (
 )
 
 func TestGetOriginalURL(t *testing.T) {
-	// make a dummy reques
-	response, err := http.Get("http://localhost:8000/v1/short/1")
+    // make a dummy reques
+    response, err := http.Get("http://localhost:8000/v1/short/1")
  
+    if http.StatusOK != response.StatusCode {
+    	t.Errorf("Expected response code %d. Got %d\n", http.StatusOK, response.StatusCode)
+    }
+
+    if err != nil {
+    	t.Errorf("Encountered an error:", err)
+    }
+}
