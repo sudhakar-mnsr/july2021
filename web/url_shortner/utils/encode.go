f6bda16143e601f3c402bbcdda8788e9f341bb8b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-11 11:34:50 +0530

    examples

--- web/url_shortner/utils/encode.go
+++ web/url_shortner/utils/encode.go
@@ -7,3 +7,10 @@ import (
 
 const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const b = 62
+
+// Function encodes the given database ID to a base62 string
+func ToBase62(num int) string {
+	r := num % b
+	res := string(base[r])
+	div := num / b
+	q := int(math.Floor(float64(div)))
