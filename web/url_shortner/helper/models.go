1116edd866a6f845f916e70c9a52396d39ac6d19
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-12 23:04:43 +0530

    examples

--- web/url_shortner/helper/models.go
+++ web/url_shortner/helper/models.go
@@ -6,3 +6,11 @@ import (
 
 	_ "github.com/lib/pq" // sql behavior modified
 )
+
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "gituser"
+	password = "passme"
+	dbname   = "mydb"
+)
