9ca85c49331e9a3dc2b0bf3bce8d6d1694636ea9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-16 22:10:19 +0530

    examples

---
+++ web/jsonstore/helper/helper.go
@@ -0,0 +1,16 @@
+package helper
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "gituser"
+	password = "passme123"
+	dbname   = "mydb"
+)
