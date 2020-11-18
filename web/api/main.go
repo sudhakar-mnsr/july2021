01e6ec018fa107fa34a5447e7667bb1d46c6366f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-18 14:15:57 +0530

    examples

--- web/api/main.go
+++ web/api/main.go
@@ -10,3 +10,10 @@ import (
 var GITHUB_TOKEN = os.Getenv("GITHUB_TOKEN")
 var requestOptions = &grequests.RequestOptions{Auth: []string{GITHUB_TOKEN, "x-oauth-basic"}}
 
+type Repo struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Forks    int    `json:"forks"`
+	Private  bool   `json:"private"`
+}
