759743e3ad715512fdf0814249f203e24630ace0
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-18 14:18:49 +0530

    examples

--- web/tools/gittool.go
+++ web/tools/gittool.go
@@ -14,3 +14,16 @@ import (
 var GITHUB_TOKEN = os.Getenv("GITHUB_TOKEN")
 var requestOptions = &grequests.RequestOptions{Auth: []string{GITHUB_TOKEN, "x-oauth-basic"}}
 
+// Struct for holding response of repositories fetch API
+type Repo struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Forks    int    `json:"forks"`
+	Private  bool   `json:"private"`
+}
+
+// Structs for modelling JSON body in create Gist
+type File struct {
+	Content string `json:"content"`
+}
