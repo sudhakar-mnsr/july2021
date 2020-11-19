66469b1ff63392764e182dc38a07756582c99c54
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-19 18:31:09 +0530

    examples

--- web/tools/cli1.go
+++ web/tools/cli1.go
@@ -6,3 +6,15 @@ import (
 
 	"github.com/urfave/cli"
 )
+
+func main() {
+	// Create new app
+	app := cli.NewApp()
+
+	// add flags with three arguments
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "name",
+			Value: "stranger",
+			Usage: "your wonderful name",
+		},
