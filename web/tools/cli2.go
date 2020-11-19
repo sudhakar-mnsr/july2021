6b83783d681828dfdb8485c89ec8792f24d3125a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-19 18:33:02 +0530

    examples

--- web/tools/cli2.go
+++ web/tools/cli2.go
@@ -6,3 +6,15 @@ import (
 
 	"github.com/urfave/cli"
 )
+
+func main() {
+	app := cli.NewApp()
+	// define flags
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "save",
+			Value: "no",
+			Usage: "Should save to database (yes/no)",
+		},
+	}
+
