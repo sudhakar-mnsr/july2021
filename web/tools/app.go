02fd2d8ad019be80519823520e4d1f271ac1fbff
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-25 14:37:55 +0530

    examples

--- web/tools/app.go
+++ web/tools/app.go
@@ -459,3 +459,19 @@ func (a *App) appendFlag(flag Flag) {
 		a.Flags = append(a.Flags, flag)
 	}
 }
+
+// Author represents someone who has contributed to a cli project.
+type Author struct {
+	Name  string // The Authors name
+	Email string // The Authors email
+}
+
+// String makes Author comply to the Stringer interface, to allow an easy print in the templating process
+func (a Author) String() string {
+	e := ""
+	if a.Email != "" {
+		e = " <" + a.Email + ">"
+	}
+
+	return fmt.Sprintf("%v%v", a.Name, e)
+}
