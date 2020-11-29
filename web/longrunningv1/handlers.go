2a15fc95217e01c687a57a47a6c868fbfb8b6777
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-29 23:04:55 +0530

    examples

--- web/longrunningv1/handlers.go
+++ web/longrunningv1/handlers.go
@@ -10,3 +10,10 @@ import (
 	"github.com/google/uuid"
 	"github.com/streadway/amqp"
 )
+
+// JobServer holds handler functions
+type JobServer struct {
+	Queue   amqp.Queue
+	Channel *amqp.Channel
+	Conn    *amqp.Connection
+}
