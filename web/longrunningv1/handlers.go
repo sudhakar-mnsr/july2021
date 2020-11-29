4985363f93b10ec756d4ff9f1122b99184dc4766
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-29 23:06:22 +0530

    examples

--- web/longrunningv1/handlers.go
+++ web/longrunningv1/handlers.go
@@ -51,3 +51,11 @@ func (s *JobServer) asyncCallbackHandler(w http.ResponseWriter, r *http.Request)
 	})
 	handleError(err, "JSON body creation failed")
 
+	if s.publish(jsonBody) == nil {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonBody)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
