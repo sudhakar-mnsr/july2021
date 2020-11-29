c7aee7e41184b27e3e70f39740b1264d3d6cd472
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-29 23:08:22 +0530

    examples

---
+++ web/longrunningv1/worker.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"encoding/json"
+
+	"github.com/Hands-On-Restful-Web-services-with-Go/chapter9/longRunningTaskV1/models"
+	"github.com/streadway/amqp"
+)
