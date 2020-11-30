84705fbf31d19cf3649ba0b87c9469a62f62b53b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-11-30 21:11:53 +0530

    examples

--- web/longrunningv2/main.go
+++ web/longrunningv2/main.go
@@ -9,3 +9,12 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/streadway/amqp"
 )
+
+const queueName string = "jobQueue"
+const hostString string = "127.0.0.1:8000"
+
+func handleError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
