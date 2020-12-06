1d876210cac11617cb7d37826fa8244d5f5b4dc1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-06 21:45:52 +0530

    examples

--- web/longrunningv2/job.go
+++ web/longrunningv2/job.go
@@ -6,3 +6,9 @@ import (
 	"github.com/google/uuid"
 )
 
+// Job represents UUID of a Job
+type Job struct {
+	ID        uuid.UUID   `json:"uuid"`
+	Type      string      `json:"type"`
+	ExtraData interface{} `json:"extra_data"`
+}
