468bf1bf5166fef1457b43c02525b6887d4adb5c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-09 13:51:01 +0530

    examples

--- graphql/server.go
+++ graphql/server.go
@@ -6,3 +6,13 @@ import (
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 )
+
+// Player holds player response
+type Player struct {
+	ID             int      `json:"int"`
+	Name           string   `json:"name"`
+	HighScore      int      `json:"highScore"`
+	IsOnline       bool     `json:"isOnline"`
+	Location       string   `json:"location"`
+	LevelsUnlocked []string `json:"levelsUnlocked"`
+}
