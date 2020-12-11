629f1169b6078c4fce42df8f5c368ec6c02cfac5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-11 21:15:38 +0530

    examples

--- async/client.go
+++ async/client.go
@@ -13,3 +13,9 @@ func ProcessEvent(ctx context.Context, event *proto.Event) error {
 	log.Println("Got alert:", event)
 	return nil
 }
+
+func main() {
+	// Create a new service
+	service := micro.NewService(micro.Name("weather_client"))
+	// Initialise the client and parse command line flags
+	service.Init()
