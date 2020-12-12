766304772066a94605a0e115c969a06ddcac1786
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-12 23:44:33 +0530

    examples

---
+++ async/server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	proto "github.com/Hands-On-Restful-Web-services-with-Go/chapter11/asyncService/proto"
+	micro "github.com/micro/go-micro"
+)
+
