167e60879f5db9aed299e1fdad81368f57485022
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-14 21:19:59 +0530

    examples

--- encrypt/handler.go
+++ encrypt/handler.go
@@ -5,3 +5,12 @@ import (
 
 	proto "github.com/Hands-On-Restful-Web-services-with-Go/chapter11/encryptService/proto"
 )
+
+// Encrypter holds the information about methods
+type Encrypter struct{}
+
+// Encrypt converts a message into cipher and returns response
+func (g *Encrypter) Encrypt(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
+	rsp.Result = EncryptString(req.Key, req.Message)
+	return nil
+}
