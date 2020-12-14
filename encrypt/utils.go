0d3cd775cb4bba63aa3b6d5d48f0ed504a2c21b5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-14 21:21:06 +0530

    examples

--- encrypt/utils.go
+++ encrypt/utils.go
@@ -6,3 +6,9 @@ import (
 	"encoding/base64"
 )
 
+
+/* Initialization vector for the AES algorithm
+   More details visit this link https://en.wikipedia.org/wiki/Advanced_Encryption_Standard */
+var initVector = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+
+// EncryptString encrypts the string with given key
