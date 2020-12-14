d5a52502ee5c57fcac497189a8fbbf814c889ba5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-14 21:20:19 +0530

    examples

--- encrypt/handler.go
+++ encrypt/handler.go
@@ -14,3 +14,9 @@ func (g *Encrypter) Encrypt(ctx context.Context, req *proto.Request, rsp *proto.
 	rsp.Result = EncryptString(req.Key, req.Message)
 	return nil
 }
+
+// Decrypt converts a cipher into message and returns response
+func (g *Encrypter) Decrypt(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
+	rsp.Result = DecryptString(req.Key, req.Message)
+	return nil
+}
