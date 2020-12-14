ca7d22c7d8092f29d9582ec1327c0a3c8e2ec663
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-14 21:21:26 +0530

    examples

--- encrypt/utils.go
+++ encrypt/utils.go
@@ -12,3 +12,9 @@ import (
 var initVector = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
 
 // EncryptString encrypts the string with given key
+func EncryptString(key, text string) string {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+	plaintext := []byte(text)
