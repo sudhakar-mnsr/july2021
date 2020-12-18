7c43fdf5dedec912c93ac6c43a88bbf497926f0f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-18 23:18:40 +0530

    examples

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -35,3 +35,12 @@ resource "aws_api_gateway_integration" "test" {
    type                    = "HTTP"
    uri                     = "http://${aws_instance.api_server.public_dns}/api/books"
  }
+
+// Integration response configuration
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  resource_id = aws_api_gateway_rest_api.test.root_resource_id
+  http_method = aws_api_gateway_method.test.http_method
+
+  status_code = aws_api_gateway_method_response.test.status_code
+}
