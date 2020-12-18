750a1e1b3a19b4d6cb0de9bb403a0d24455086eb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-18 23:17:49 +0530

    examples

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -15,3 +15,13 @@ resource "aws_api_gateway_method" "test" {
 
    authorization = "NONE"
  }
+
+// Method response configuration
+resource "aws_api_gateway_method_response" "test" {
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  resource_id = aws_api_gateway_rest_api.test.root_resource_id
+  http_method = aws_api_gateway_method.test.http_method
+
+  status_code = "200"
+}
+
