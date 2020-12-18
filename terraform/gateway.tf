c8cde0730e1f843eac985bd6f4b621eec7d9ea78
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-18 23:16:03 +0530

    examples

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -7,3 +7,11 @@ resource "aws_api_gateway_rest_api" "test" {
   }
 }
 
+// Method request configuration
+resource "aws_api_gateway_method" "test" {
+   rest_api_id   = aws_api_gateway_rest_api.test.id
+   resource_id   = aws_api_gateway_rest_api.test.root_resource_id
+   http_method   = "GET"
+
+   authorization = "NONE"
+ }
