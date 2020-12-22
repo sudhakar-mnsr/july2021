27ddc265de7d193a0c01137972a615eb810caff3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-22 23:02:48 +0530

    examples

--- auth/simpleauth/main.go
+++ auth/simpleauth/main.go
@@ -9,3 +9,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 )
+
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
+var users = map[string]string{"naren": "passme", "admin": "password"}
