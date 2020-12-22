f7ec5a80980693d7e2f01eaf7e055f1f667e61cb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-22 22:58:02 +0530

    examples

--- auth/main.go
+++ auth/main.go
@@ -97,3 +97,11 @@ func main() {
 	r.HandleFunc("/healthcheck", HealthcheckHandler)
 	http.Handle("/", r)
 	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
