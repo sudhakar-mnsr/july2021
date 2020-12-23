beebc85b6cb3b42b8863bfaa525d412584d8f88e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-12-23 14:39:35 +0530

    examples

--- auth/simpleauthredis/main.go
+++ auth/simpleauthredis/main.go
@@ -10,3 +10,5 @@ import (
 	redistore "gopkg.in/boj/redistore.v1"
 )
 
+var store, err = redistore.NewRediStore(10, "tcp", ":6379", "", []byte(os.Getenv("SESSION_SECRET")))
+var users = map[string]string{"naren": "passme", "admin": "password"}
